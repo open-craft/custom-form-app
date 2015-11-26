30d99eb344d2d44bca9da5b60b411b688e2dc3e0
Braden MacDonald <braden@opencraft.com>
2015-11-25 22:59:29 -0800

    Fix validation errors, add to admin

--- custom_reg_form/forms.py
+++ custom_reg_form/forms.py
@@ -5,7 +5,12 @@ class ExtraInfoForm(ModelForm):
     """
     The fields on this form are derived from the ExtraInfo model in models.py.
     """
-    pass
+    def __init__(self, *args, **kwargs):
+        super(ExtraInfoForm, self).__init__(*args, **kwargs)
+        self.fields['favorite_movie'].error_messages = {
+            "required": u"Please tell us your favorite movie.",
+            "invalid": u"We're pretty sure you made that movie up.",
+        }
 
     class Meta(object):
         model = ExtraInfo
